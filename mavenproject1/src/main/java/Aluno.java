c44a0a775a8b520d6ee8c946abb59971caf948c0
Kaua10710 <kaduzerah06@gmail.com>
2024-12-08 16:44:56 -0300

    Inicialização

---
+++ mavenproject1/src/main/java/Aluno.java
@@ -0,0 +1,18 @@
+/**
+ *Classe Aluno representa um aluno com métodos para calcular a média
+ * aritmética de suas notas e verificar a aprovação com base na média mínima.
+ * @author Kaua Luiz
+ */
+public class Aluno {
+    private String nome;
+    
+    /**
+     * Construtor da classe Aluno
+     * 
+     * @param nome Nome do aluno.
+     */
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+    
+}
