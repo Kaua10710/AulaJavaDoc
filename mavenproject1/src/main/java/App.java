a6e5632619469b620fdddbe5d2656f6bd8a7ce12
Kaua10710 <kaduzerah06@gmail.com>
2024-12-08 17:35:48 -0300

    Create App.java

---
+++ mavenproject1/src/main/java/App.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/**
+ * 
+ * @authork Kaua Luiz
+ * Classe principal para gerenciar dados de alunos e calcular médias.
+ */
+
+public class App {
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Aluno> alunos = new ArrayList<>();
+      }
+}
