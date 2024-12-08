d701963a175861f1c8d9217fc05078e6f3e381fa
Kaua10710 <kaduzerah06@gmail.com>
2024-12-08 16:48:21 -0300

    calculando

--- mavenproject1/src/main/java/Aluno.java
+++ mavenproject1/src/main/java/Aluno.java
@@ -14,5 +14,7 @@ public class Aluno {
     public Aluno(String nome) {
         this.nome = nome;
     }
-    
+    public double calcularMedia(double nota1, double nota2, double nota3, double nota4){
+        return (nota1 + nota2 + nota3 + nota4) / 4.0;
+    }
 }
