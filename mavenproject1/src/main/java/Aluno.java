48e25c83db7cc558f1418f57d7605d7fd44fcc18
Kaua10710 <kaduzerah06@gmail.com>
2024-12-08 16:53:18 -0300

    Update Aluno.java

--- mavenproject1/src/main/java/Aluno.java
+++ mavenproject1/src/main/java/Aluno.java
@@ -26,8 +26,21 @@ public class Aluno {
     public double calcularMedia(double nota1, double nota2, double nota3, double nota4){
         return (nota1 + nota2 + nota3 + nota4) / 4.0;
     }
-    
+     /**
+     * Verifica se o aluno foi aprovado ou reprovado com base na média.
+     *
+     * @param media A média calculada do aluno.
+     * @return "Aprovado" se a média for maior ou igual a 6.0, caso contrário, "Reprovado".
+     */
     public String verificarAprovacao(double media) {
         return media >= 6.0 ? "Aprovado" : "Reprovado";
+    }
+    /**
+     * Retorna o nome do aluno.
+     * 
+     * @return nome do aluno
+     */
+    public String getNome() {
+        return nome;
     }
 }
